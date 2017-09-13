35315e726aafbf83a86da23ba7ccccea9183f99b
Ling Tran <lxiutran@gmail.com>
2017-09-13 13:53:17 -0500

    Manage and update component state

--- app/index.js
+++ app/index.js
@@ -2,44 +2,13 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var PropTypes = require('prop-types');
 require('./index.css');
+var App = require('./components/App')
 
-// class App extends React.Component {
-//   render () {
-//     return (
-//       <div>
-//         Hello World! React training
-//       </div>
-//     )
-//   }
-// }
 
-class Badge extends React.Component {
-  render () {
-    return (
-      <div>
-        <img
-          src={this.props.img}
-          alt='Avatar'
-          style={{width: 100, height: 100}}
-        />
-        <h1>Name: {this.props.name}</h1>
-        <h3>username: {this.props.username} </h3>
-      </div>
-    )
-  }
-}
 
-Badge.propTypes = {
-  img: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  username: PropTypes.string.isRequired
-}
 
 ReactDOM.render(
-  <Badge
-    name="Some name"
-    username="someusername"
-    img="https://avatars2.githubusercontent.com/u/5829095?v=4&s=460"
+  <App
   />,
   document.getElementById('app')
 );
